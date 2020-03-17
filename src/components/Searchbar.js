7f27d6a66e676ff707b6d19b73e23f0dd748430d
Chí Nguyễn Đắc <dacchi6761@gmail.com>
2020-03-17 07:36:28 +0700

    Get data by search

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+
+import { PokemonContext } from '../contexts/PokemonContext'
 
 function Searchbar(props) {
+    const { displayDetailPokemonData } = useContext(PokemonContext);
+    const [searchText, setSearchText] = useState('');
+
     return (
         <div className='container mt-5 d-flex justify-content-center align-items-center'>
-            <input type='text' placeholder='Enter pokémon name or ID'></input>
-            <button className='searchBtn ml-2'>Search</button>
+            <input type='text' onChange={(e) => setSearchText(e.target.value)} placeholder='Enter pokémon name or ID'></input>
+            <button className='searchBtn ml-2' onClick={() => displayDetailPokemonData(searchText)}>Search</button>
         </div>
     )
 }
--- src/contexts/PokemonContext.js
+++ src/contexts/PokemonContext.js
@@ -6,6 +6,7 @@ export const PokemonContext = React.createContext();
 
 export function PokemonProvider(props) {
     const [pokemonDataList, setPokemonDataList] = useState([]);
+    const [pokeNameList, setPokemonNameList] = useState(null);
 
     async function fetchPokemonDataList(beginId, endId) {
         const urlList = [];
@@ -17,15 +18,39 @@ export function PokemonProvider(props) {
             .catch(err => console.log(err));
     }
 
-    useEffect(() => {
+    async function getPokemonData() {
+        axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=964')
+            .then(result => setPokemonNameList(result.data.results))
+            .catch(err => console.log(err));
         fetchPokemonDataList(1, 20);
+    }
+
+    async function displayDetailPokemonData(searchText) {
+        let listPokemon;
+        if (isNaN(searchText)) {
+            listPokemon = pokeNameList.filter(item => item.name.toLowerCase().includes(searchText.toLowerCase()))
+                .map(item => `https://pokeapi.co/api/v2/pokemon/${item.name}`);
+        } else {
+            listPokemon = (searchText <= 0 || searchText > 964) ? [] : [`https://pokeapi.co/api/v2/pokemon/${pokeNameList[searchText - 1].name}`];
+        }
+        console.log(listPokemon)
+        if (listPokemon.length === 0) {
+            console.log('No result!')
+        } else {
+            await Promise.all(listPokemon.map(item => axios.get(item)))
+                .then(result => console.log(result))
+                .catch(err => console.log(err))
+        }
+    }
+
+    useEffect(() => {
+        getPokemonData()
     }, [])
-
-
 
     const contextValue = {
         pokemonDataList,
-        fetchPokemonDataList
+        fetchPokemonDataList,
+        displayDetailPokemonData,
     }
 
     return (
