6a41d58205a9e485ffe10bf90b0bf1e3a1148f6f
chinguyen98 <dacchi6761@gmail.com>
2020-09-04 10:34:22 +0700

    Updates

---
+++ precache-manifest.359909775915d87084c7e7d585ccb59c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aefa3400fbd5a61e3c6e72491d8cfb3",
+    "url": "/pokedex-webapp/index.html"
+  },
+  {
+    "revision": "1b5dbe1e6dc12cc12b6e",
+    "url": "/pokedex-webapp/static/css/main.38327560.chunk.css"
+  },
+  {
+    "revision": "f7f12c655cbe2a1768ad",
+    "url": "/pokedex-webapp/static/js/2.212cb0a4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/pokedex-webapp/static/js/2.212cb0a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5dbe1e6dc12cc12b6e",
+    "url": "/pokedex-webapp/static/js/main.847fce97.chunk.js"
+  },
+  {
+    "revision": "ce69a376c2908a431163",
+    "url": "/pokedex-webapp/static/js/runtime-main.27a06efe.js"
+  },
+  {
+    "revision": "74a6852e22caefcbeb4e20047641e85e",
+    "url": "/pokedex-webapp/static/media/pokedexLogo.74a6852e.png"
+  }
+]);
